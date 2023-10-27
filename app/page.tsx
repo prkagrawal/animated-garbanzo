7fb394c8be60e103f55194a12d06a77f6671e242
Josh Cox <josh@webhosting.coop>
2023-10-27 16:32:10 -0500

    ending chapter 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 import Link from 'next/link';
 export default function Page() {
@@ -32,9 +33,19 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-      <img
-        src="/hero.png"
+      <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop and mobile versions"
+      />
+      <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
       />
         </div>
       </div>
