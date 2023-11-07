ce534e5f49675f1d4752a1bdc3f9b78850edebf1
Josh Cox <josh@webhosting.coop>
2023-11-07 10:51:26 -0600

    typo

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -37,7 +37,7 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customerId}
+              defaultValue={invoice.customer_id}
             >
               <option value="" disabled>
                 Select a customer
